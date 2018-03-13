dc8d60e0dde71f44cbc74f1406d61e7763988917
Ryanbritt <britt.ryant@gmail.com>
2018-03-13 13:48:45 -0400

    added lifting state example

--- src/components/ComponentB.js
+++ src/components/ComponentB.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-
 class ComponentB extends Component {
+constructor(props){
+    super(props);
+    this.state = {
+      youCanNameMeWhateverTwo : 0
+    }
+  }
+  handleClick(e){
+    e.preventDefault();
+    this.props.propC();
+  }
   render(){
     return(
+    <div>
       <h1>I am Component B!</h1>
+      <input type="submit" onClick={(e)=>this.handleClick(e)} />
+    </div>
     )
   }
 }
-
 export default ComponentB;
